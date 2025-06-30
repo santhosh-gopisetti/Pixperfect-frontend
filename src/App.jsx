fa8de9482939b05af18daf9f3c639b1adbacbbca
santhosh-gopisetti <santhoshgopisetti258@gmail.com>
2025-06-30 21:28:01 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ import Images from './components/Images.jsx';
 import Upload from './components/Upload';
 import './App.css';
 
-// ProtectedRoute to restrict access
 function ProtectedRoute({ isAuthenticated, children }) {
   const navigate = useNavigate();
   const location = useLocation();
@@ -36,7 +35,11 @@ function App() {
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    setIsAuthenticated(!!token);
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
   }, []);
 
   const handleLogout = () => {
@@ -73,22 +76,16 @@ function App() {
           <Route
             path="/"
             element={
-              <div
-                className="home-container with-background"
-                style={{ backgroundImage: 'url(/RRR.jpg)' }}
-              >
+              <div className="home-container with-background">
                 <h2>Welcome to PixPerfect</h2>
                 <p>Make your frames visually more appealing !!</p>
-                <div className="editing-images-grid">
-                  <div className="editing-image-card">
-                    <img src="/KHALEJA DUNE.jpg" alt="Banner 1" />
-                  </div>
-                  <div className="editing-image-card">
-                    <img src="/Kalki 70s poster.jpg" alt="Banner 2" />
-                  </div>
-                  <div className="editing-image-card">
-                    <img src="/JVAS DEVARA.jpg" alt="Banner 3" />
-                  </div>
+                <button className="cta-button" onClick={() => navigate('/upload')}>
+                  Upload Now
+                </button>
+                <div className="homepage-images">
+                  <img src="/KHALEJA DUNE.jpg" alt="Banner 1" className="home-banner" />
+                  <img src="/Kalki 70s poster.jpg" alt="Banner 2" className="home-banner" />
+                  <img src="/JVAS DEVARA.jpg" alt="Banner 3" className="home-banner" />
                 </div>
               </div>
             }
